b14b630cb652a347ebbae314ba967aac38279dc1
antoniotassone94 <antonio.tassone@studio.unibo.it>
2023-12-08 17:16:47 +0100

    resolved bug in the sending of the messages.

--- backend/src/models/message.model.ts
+++ backend/src/models/message.model.ts
@@ -1,12 +1,16 @@
-import {Schema,model} from "mongoose";
+import {Date,Schema,model} from "mongoose";
 
 const messageModel = new Schema<MessageSchema>({
-    text:{type:String,required:true}
+    text:{type:String,required:true},
+    sender:{type:String,required:true},
+    date:{type:Date,required:true}
 })
 
 export interface MessageSchema{
     save(): MessageSchema|PromiseLike<MessageSchema|null> | null
     text:String
+    sender:String
+    date:Date
 }
 
 export const Message = model<MessageSchema>("message",messageModel)
--- frontend/src/app/models/message.model.ts
+++ frontend/src/app/models/message.model.ts
@@ -1,10 +1,14 @@
 export class MessageModel{
     private _id:string;
     private _text:string;
+    private _sender:string;
+    private _date:Date;
 
     constructor(){
         this._id = "";
         this._text = "";
+        this._sender = "";
+        this._date = new Date();
     }
 
     public get id():string{
@@ -21,5 +25,21 @@ export class MessageModel{
 
     public set text(_text:string){
         this._text = _text;
+    }
+
+    public get sender():string{
+        return this._sender;
+    }
+
+    public set sender(_sender:string){
+        this._sender = _sender;
+    }
+
+    public get date():Date{
+        return this._date;
+    }
+
+    public set date(_date:Date){
+        this._date = _date;
     }
 }
